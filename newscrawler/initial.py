c29c845e09cd2be1766d9a06094ed5a21aede6fe
Jonathan Hassler <JBH168@gmail.com>
2016-05-26 18:10:30 +0200

    fixed relative paths, get URLInput json file path from config

--- newscrawler/initial.py
+++ newscrawler/initial.py
@@ -23,19 +23,23 @@ from newscrawler.config import JsonConfig
 
 from newscrawler.crawler.helper import helper
 
+
 class initial(object):
     cfg = None
     json = None
     log = None
     process = None
     helper = None
+
     def __init__(self):
         self.cfg = CrawlerConfig.get_instance()
-        self.cfg.setup("./newscrawler.cfg")
+        self.cfg.setup(self.get_abs_file_path("./newscrawler.cfg"))
         self.log = logging.getLogger(__name__)
         self.log.info("Config initalized - Further initialisation.")
+
+        urlinput_file_path = self.cfg.section('Files')['urlinput']
         self.json = JsonConfig.get_instance()
-        self.json.setup("./input_data.json")
+        self.json.setup(self.get_abs_file_path(urlinput_file_path))
 
         self.helper = helper()
 
@@ -51,13 +55,22 @@ class initial(object):
             config=self.cfg,
             json=self.json)
 
+    def get_abs_file_path(self, rel_file_path):
+        # for the following three lines of code, see:
+        # http://stackoverflow.com/questions/7165749/open-file-in-a-relative-location-in-python
+        script_dir = os.path.dirname(__file__)  # absolute dir the script is in
+        abs_file_path = os.path.abspath(
+                            os.path.join(script_dir, rel_file_path))
+
+        if not os.path.exists(abs_file_path):
+            self.log.error(abs_file_path + " does not exist")
+
+        return abs_file_path
 
 
 if __name__ == "__main__":
     initial()
 
-
-# TODO: json parser to be called
 
 # decide which webcrawler to call and pass arguments along
 # http://doc.scrapy.org/en/latest/topics/spiders.html#spider-arguments
