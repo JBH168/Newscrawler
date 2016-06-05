b8bcb1109ba70c6e1d7b801019d15bc8df90a84a
Matthew Sharinghousen <matt.sharinghousen@gmail.com>
2016-06-05 19:28:48 +0000

    Added *tested* DB pipeline and item set. Item stuff is commented out in the crawler def, because there was an unknown error. Needs checking.

--- newscrawler/crawler/pipelines.py
+++ newscrawler/crawler/pipelines.py
@@ -4,69 +4,98 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import MySQLdb
 from scrapy.exceptions import DropItem
+import mysql.connector
+import datetime
 import os.path
 import hashlib
-
-class Heuristics(object):
-
-	def __init__(self):
-
-	def process_item(self, item, spider):
-		return item
 
 class DatabaseStorage(object):
 
 	#init database connection
-	#doc: http://mysql-python.sourceforge.net/MySQLdb.html#functions-and-attributes
-	def __init__(self):
-		self.conn = MySQLdb.connect(host='db.dbvis.de', port='3306', db='ccolon', user='ccolon', passwd='b3eY7Tep2F7Pg559Vg0W')
-		self.cursor = self.conn.cursor()
+        #doc: http://mysql-python.sourceforge.net/MySQLdb.html#functions-and-attributes
+        def __init__(self):
 
-		#Init all necessary DB queries for this pipeline
-		compareVersions = ("""SELECT * FROM CurrentVersion WHERE url=item.url""")
-		insertCurrent = ("""INSERT INTO CurrentVersion(modifiedDate,downloadDate,sourceDomain,url,hashedName,title,ancestor)""")
-		insertArchive = ("""INSERT INTO CurrentVersion(id, modifiedDate,downloadDate,sourceDomain,url,hashedName,title,ancestor)""")
-		#TODO
-		deleteFromCurrent = ("""""")
+                #Establish DB connection
+                self.conn = mysql.connector.connect(host='db.dbvis.de', port=3306, db='ccolon', user='ccolon', passwd='b3eY7Tep2F7Pg559Vg0W')
+                self.cursor = self.conn.cursor(buffered=True)
 
-	#Store item data in DB.
-	#First determine if a version of the article already exists, if so then 'migrate' the older version to the archive table.
-	#Second store the new article in the current version table
-	def process_item(self, item, spider):
+                #Init all necessary DB queries for this pipeline
+                self.compareVersions = ("SELECT * FROM CurrentVersion WHERE url=%s")
 
-		#Search the CurrentVersion table for a version of the article
-		temp = self.cursor.execute(compareVersions)
+                self.insertCurrent = ("INSERT INTO CurrentVersion(localPath,modifiedDate,downloadDate,sourceDomain,url,hashedName,title,ancestor) \
+                        VALUES (%(localPath)s, %(modifiedDate)s, %(downloadDate)s, %(sourceDomain)s, %(url)s, %(hashedName)s, %(title)s, %(ancestor)s)")
 
-		#If the article is found in CurrentVerion, then the older version needs to be migrated to the ArchiveVersion table.
-		if (temp != 0)
-			oldVersion = self.cursor.fetchone()
-			#Link the two versions via the ancestor attribute
-			item[ancestor] = oldVersion[0] #0 index is the first attribute in the table, namely id.
+                self.insertArchive = ("INSERT INTO ArchiveVersion(id,localPath,modifiedDate,downloadDate,sourceDomain,url,hashedName,title,ancestor) \
+                        VALUES (%(id)s, %(localPath)s, %(modifiedDate)s, %(downloadDate)s, %(sourceDomain)s, %(url)s, %(hashedName)s, %(title)s, %(ancestor)s)")
 
-			#Add the older version to the archive table
-			try:
-				self.cursor.execute("""insertArchive VALUES (%s, %s, %s, %s, %s, %s, %u)""", (oldVersion[0], oldVersion[1], oldVersion[2], oldVersion[3], oldVersion[4], oldVersion[5], oldVersion[6], oldVersion[7]))
-				self.conn.commit()
+                self.deleteFromCurrent = ("DELETE FROM CurrentVersion WHERE url = %s")
 
-			except MySQLdb.Error, e:
-				print "Error %d: %s" % (e.args[0], e.args[1])
+        #Store item data in DB.
+        #First determine if a version of the article already exists, if so then 'migrate' the older version to the archive table.
+        #Second store the new article in the current version table
+        def process_item(self, item, spider):
 
-			#Remove the older version from the CurrentVersion table
-			#TODO
+                #Search the CurrentVersion table for a version of the article
+                try:
+                        temp = self.cursor.execute(self.compareVersions, (item['url'],))
 
-		#Add the new version of the article to the CurrentVersion table
+                except mysql.connector.Error as err:
+                        print("Something went wrong in query: {}".format(err))
+
+
+                ## If there is an existing article with the same URL, then move it to the ArchiveVersion table, and delete it from the CurrentVersion table
+                oldVersion = self.cursor.fetchone()
+                if oldVersion != None:
+                        data_test1 = {
+                                'id': oldVersion[0],
+                                'localPath': oldVersion[1],
+                                'modifiedDate': oldVersion[2],
+                                'downloadDate': oldVersion[3],
+                                'sourceDomain': oldVersion[4],
+                                'url': oldVersion[5],
+                                'hashedName': oldVersion[6],
+                                'title': oldVersion[7],
+                                'ancestor': oldVersion[8],
+                        }
+                        try:
+                                self.cursor.execute(self.deleteFromCurrent, (oldVersion[5],))
+                                self.conn.commit()
+                        except mysql.connector.Error as err:
+                                print("Something went wrong in delete: {}".format(err))
+
+
+                        try:
+                                self.cursor.execute(self.insertArchive, data_test1)
+                                self.conn.commit()
+                        except mysql.connector.Error as err:
+                                print("Something went wrong in archive: {}".format(err))
+
+                        # Link the new version with the old
+                        item['ancestor'] = oldVersion[8]
+
+		data_test = {
+                        'localPath': item['localPath'],
+                        'modifiedDate': item['modifiedDate'],
+                        'downloadDate': item['downloadDate'],
+                        'sourceDomain': item['sourceDomain'],
+                        'url': item['url'],
+                        'hashedName': item['filename'],
+                        'title': item['title'],
+                        'ancestor': item['ancestor'],
+		}
+
+                #Add the new version of the article to the CurrentVersion table
 		try:
-			self.cursor.execute("""insertCurrent VALUES (%s, %s, %s, %s, %s, %s, %u)""", (item[modifiedDate], item[downloadDate], item[sourceDomain], item[url], item[hashedName], item[title], item[ancestor]))
-			self.conn.commit()
+                        self.cursor.execute(self.insertCurrent, data_test)
+                        self.conn.commit()
 
-		except MySQLdb.Error, e:
-			print "Error %d: %s" % (e.args[0], e.args[1])
+                except mysql.connector.Error as err:
+                        print("Something went wrong in commit: {}".format(err))
 
-		#Close DB connection - garbage collection
-		self.cursor.close()
-		return item
+                #Close DB connection - garbage collection
+                self.conn.close()
+                return item
 
 class LocalStorage(object):
 	#Init the local file including determining path and filename
--- newscrawler/crawler/spiders/SitemapCrawler.py
+++ newscrawler/crawler/spiders/SitemapCrawler.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.item import Item, Field
 
+class NewscrawlerItem(scrapy.Item):
+    # ID of the article in the DB
+    id = scrapy.Field()
+    # Path of the file on the local filesystem
+    localPath = scrapy.Field()
+    # When the article was last modified
+    modifiedDate = scrapy.Field()
+    # When the article was downloaded
+    downloadDate = scrapy.Field()
+    # Root domain from which the article came
+    sourceDomain = scrapy.Field()
+    url = scrapy.Field()
+    # Hashed filename for local storage
+    filename = scrapy.Field()
+    # Title of the article
+    title = scrapy.Field()
+    # Older version of the article in the DB, if exists
+    ancestor = scrapy.Field()
+    # Raw HTML data for local storage
+    html = scrapy.Field()
 
 class SitemapCrawler(scrapy.spiders.SitemapSpider):
     name = "SitemapCrawler"
@@ -36,6 +57,17 @@ class SitemapCrawler(scrapy.spiders.SitemapSpider):
         # heuristics
         if self.helper.heuristics.is_article(response):
             self.helper.download.save_webpage(response)
+
+	#article = NewscrawlerItem()
+        #article['localPath'] = '/testItem/'
+        #article['modifiedDate'] = '1000-01-01 00:00:00'
+        #article['downloadDate'] = '1000-01-01 00:00:00'
+        #article['sourceDomain'] = 'zeit.de'
+        #article['url'] = 'zeit.de/secret_files/'
+        #article['filename'] = '123123'
+        #article['title'] = 'Hello World'
+        #article['ancestor'] = 'NULL'
+	#return article
 
     # in case anything needs to be done after a crawler is done
     # def closed(self, reason):
