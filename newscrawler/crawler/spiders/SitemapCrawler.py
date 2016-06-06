148386d2466a7683971f3da545a1a25fd9dc95c9
Jonathan Hassler <JBH168@gmail.com>
2016-06-06 17:24:03 +0200

    formatting and timestamp

--- newscrawler/crawler/spiders/SitemapCrawler.py
+++ newscrawler/crawler/spiders/SitemapCrawler.py
@@ -6,6 +6,7 @@ import shutil
 import os
 from newscrawler.crawler.items import NewscrawlerItem
 
+
 class SitemapCrawler(scrapy.spiders.SitemapSpider):
     name = "SitemapCrawler"
     allowed_domains = None
@@ -30,32 +31,35 @@ class SitemapCrawler(scrapy.spiders.SitemapSpider):
         super(SitemapCrawler, self).__init__(*args, **kwargs)
 
     def parse(self, response):
-	
-	### TODO: this code-block breaks the pipeline (cause of yield), can be replaced with a Rule && LinkExtractor
-	### http://doc.scrapy.org/en/latest/topics/link-extractors.html#topics-link-extractors
-        #if self.config.section('Crawler')['recursivesitemap']:   
-	# Recursivly crawl all URLs on the current page
-            #for href in response.css("a::attr('href')"):
-             #   url = response.urljoin(href.extract())
-              #  yield scrapy.Request(url, callback=self.parse)
 
-	if self.helper.heuristics.is_article(response):
-    		article = NewscrawlerItem()
-        	article['localPath'] = self.helper.savepath_parser.get_savepath(response.url)
-		article['modifiedDate'] = '1000-01-01 00:00:00' #####Filler
-        	article['downloadDate'] = '1000-01-01 00:00:00' #####Filler
-		article['sourceDomain'] = self.allowed_domains[0].encode("utf-8") 
-		article['url'] = response.url 
-        	article['filename'] = '123123' #######DO we need this?? 
-        	article['title'] = 'temp_title' #response.selector.xpath("//h1/text()").extract() 
-        	article['ancestor'] = 'NULL' 
-		article['spiderResponse'] = response
-		return article
+        # TODO: this code-block breaks the pipeline (cause of yield),
+        #       can be replaced with a Rule && LinkExtractor
+        # http://doc.scrapy.org/en/latest/topics/link-extractors.html#topics-link-extractors
+        # if self.config.section('Crawler')['recursivesitemap']:
+        # Recursivly crawl all URLs on the current page
+            # for href in response.css("a::attr('href')"):
+                # url = response.urljoin(href.extract())
+                # yield scrapy.Request(url, callback=self.parse)
 
-    ### TODO: Causes errors on *nix
-    ### 
-    #def closed(self, reason):
-    #    if self.config.section('Files')['removejobdironfinishedsignal'] \
-    #            and reason == 'finished':
-    #        shutil.rmtree(os.path.abspath(os.path.join(
-    #            self.cwd, self.config.section('Scrapy')['jobdir'])))
+        if self.helper.heuristics.is_article(response):
+            timestamp = time.strftime('%y-%m-%d %H:%M:%S',
+                                      time.gmtime(time.time()))
+            article = NewscrawlerItem()
+            article['localPath'] = self.helper.savepath_parser \
+                .get_savepath(response.url)
+            article['modifiedDate'] = timestamp
+            article['downloadDate'] = timestamp
+            article['sourceDomain'] = self.allowed_domains[0].encode("utf-8")
+            article['url'] = response.url
+            # TODO: response.selector.xpath("//h1/text()").extract()
+            article['title'] = 'temp_title'
+            article['ancestor'] = 'NULL'
+            article['spiderResponse'] = response
+        return article
+
+    # TODO: Causes errors on *nix
+    # def closed(self, reason):
+    #     if self.config.section('Files')['removejobdironfinishedsignal'] \
+    #             and reason == 'finished':
+    #         shutil.rmtree(os.path.abspath(os.path.join(
+    #             self.cwd, self.config.section('Scrapy')['jobdir'])))
