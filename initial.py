082c8e74e7361e8515459dd55aae4bf330ae6268
movabo <moritz.bock@uni-konstanz.de>
2016-06-27 02:18:53 +0200

    Implemented graceful shutdown, daemonize bugfix
     * daemonize bugfixing: daemonize was not correctly passed to the crawler

--- initial.py
+++ initial.py
@@ -1,15 +1,7 @@
 """
-This script should be called whenever a crawl should be executed
+This script should only be executed by the newscrawler initial script iteself.
 
-The first parameter, if passed to this script,
-should be a path to a config file (*.cfg)
- - it can be absolute or relative
- - if it isn't passed along, scrapy will fall back to the default
-   newscrawler.cfg
-Another parameter that can be passed to this script is '--resume'
- - if this parameter isn't passed along, this script will delete the JOBDIR
-   defined in the config file if it does exist
- - otherwise, any crawler will be called with this JOBDIR and resume crawling
+This script starts a crawler.
 """
 
 import os
@@ -22,11 +14,6 @@ import hashlib
 
 from scrapy.crawler import CrawlerProcess
 
-from newscrawler.crawler.spiders.sitemapCrawler import sitemapCrawler
-from newscrawler.crawler.spiders.recursiveSitemapCrawler import recursiveSitemapCrawler
-from newscrawler.crawler.spiders.recursiveCrawler import recursiveCrawler
-from newscrawler.crawler.spiders.rssCrawler import rssCrawler
-
 from newscrawler.config import CrawlerConfig
 from newscrawler.config import JsonConfig
 
@@ -36,7 +23,7 @@ from scrapy.utils.log import configure_logging
 from ast import literal_eval
 
 
-class initial(object):
+class single_crawler(object):
     """
     This class is called when this script is executed
 
@@ -49,6 +36,7 @@ class initial(object):
     process = None
     helper = None
     cfg_file_path = None
+    json_file_path = None
     cfg_crawler = None
     __scrapy_options = None
     __crawer_module = "newscrawler.crawler.spiders"
@@ -56,28 +44,28 @@ class initial(object):
     shall_resume = False
     daemonize = 0
 
-    def __init__(self):
+    def __init__(self, cfg_file_path, json_file_path, site_index, shall_resume, daemonize):
         # set up logging before it's defined via the config file,
         # this will be overwritten and all other levels will be put out
         # as well, if it will be changed.
         configure_logging({"LOG_LEVEL": "CRITICAL"})
         self.log = logging.getLogger(__name__)
 
-        self.site_number = int(sys.argv[2])
-        self.shall_resume = literal_eval(sys.argv[3])
-        self.daemonize = int(sys.argv[4])
+        self.cfg_file_path = cfg_file_path
+        self.json_file_path = json_file_path
+        self.site_number = int(site_index)
+        self.shall_resume = shall_resume if isinstance(shall_resume, bool) else literal_eval(shall_resume)
+        self.daemonize = int(daemonize)
 
         # set up the config file
         self.cfg = CrawlerConfig.get_instance()
-        self.cfg_file_path = sys.argv[1]
         self.cfg.setup(self.cfg_file_path)
         self.log.info("Config initalized - Further initialisation.")
 
         # load the URL-input-json-file
         urlinput_file_path = self.cfg.section('Files')['urlinput']
         self.json = JsonConfig.get_instance()
-        self.json.setup(self.get_abs_file_path(
-            urlinput_file_path, quit_on_error=True))
+        self.json.setup(self.json_file_path)
 
         site = self.json.get_site_objects()[self.site_number]
 
@@ -146,31 +134,9 @@ class initial(object):
             self.log.info("Removed JOBDIR since '--resume' was not passed to"
                           " initial.py")
 
-    # TODO: move into a helper class; copy in initial.py
-    def get_abs_file_path(self, rel_file_path, quit_on_error=None):
-        """
-        returns the absolute file path of the given relative file path
-        to either this script or to the config file.
-
-        May throw a RuntimeError if quit_on_error is True
-        """
-        if self.cfg_file_path is not None and \
-                not self.cfg.section('General')['relativetoinitial']:
-            script_dir = os.path.dirname(self.cfg_file_path)
-        else:
-            # absolute dir this script is in
-            script_dir = os.path.dirname(__file__)
-
-        abs_file_path = os.path.abspath(
-            os.path.join(script_dir, rel_file_path))
-
-        if not os.path.exists(abs_file_path):
-            self.log.error(abs_file_path + " does not exist.")
-            if quit_on_error is True:
-                raise RuntimeError("Importet file not found. Quit.")
-
-        return abs_file_path
-
-
 if __name__ == "__main__":
-    initial()
+    single_crawler(cfg_file_path=sys.argv[1],
+                   json_file_path=sys.argv[2],
+                   site_index=sys.argv[3],
+                   shall_resume=sys.argv[4],
+                   daemonize=sys.argv[5])
