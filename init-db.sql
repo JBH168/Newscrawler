07ccee4cd102de74bb8fdb867fc14b6718071dbf
msharing <matt.sharinghousen@gmail.com>
2016-07-03 14:00:51 +0200

    updated db_init with new attribute names, DB server-side also updated.

--- init-db.sql
+++ init-db.sql
@@ -1,5 +1,5 @@
 -- Re-initalize DB-schema for the ccolon web-crawler
--- Updated: 11.06.2016 16:11
+-- Updated: 03.07.2016 14:00
 
 --
 -- Table structure for table `ArchiveVersion`
@@ -10,28 +10,18 @@ DROP TABLE IF EXISTS `ArchiveVersion`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ArchiveVersion` (
   `id` int(10) unsigned NOT NULL,
-  `localPath` varchar(255) NOT NULL,
-  `modifiedDate` datetime NOT NULL,
-  `downloadDate` datetime NOT NULL,
-  `sourceDomain` varchar(255) NOT NULL,
+  `local_path` varchar(255) NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `download_date` datetime NOT NULL,
+  `source_domain` varchar(255) NOT NULL,
   `url` varchar(2000) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `ancestor` int(10) unsigned DEFAULT NULL,
-  `descendant` int(10) unsigned DEFAULT NULL,
-  `version` int(10) unsigned NOT NULL,
-  `rssTitle` varchar(255) NOT NULL,
+  `html_title` varchar(255) NOT NULL,
+  `ancestor` int(10) unsigned NOT NULL DEFAULT 0,
+  `descendant` int(10) unsigned NOT NULL,
+  `version` int(10) unsigned NOT NULL DEFAULT 2,
+  `rss_title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ArchiveVersion`
---
-
-LOCK TABLES `ArchiveVersion` WRITE;
-/*!40000 ALTER TABLE `ArchiveVersion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ArchiveVersion` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `CurrentVersion`
@@ -42,25 +32,15 @@ DROP TABLE IF EXISTS `CurrentVersion`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `CurrentVersion` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `localPath` varchar(255) NOT NULL,
-  `modifiedDate` datetime NOT NULL,
-  `downloadDate` datetime NOT NULL,
-  `sourceDomain` varchar(255) NOT NULL,
+  `local_path` varchar(255) NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `download_date` datetime NOT NULL,
+  `source_domain` varchar(255) NOT NULL,
   `url` varchar(2000) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `ancestor` int(10) unsigned DEFAULT NULL,
-  `descendant` int(10) unsigned DEFAULT NULL,
-  `version` int(10) unsigned NOT NULL,
-  `rssTitle` varchar(255) NOT NULL,
+  `html_title` varchar(255) NOT NULL,
+  `ancestor` int(10) unsigned NOT NULL DEFAULT 0,
+  `descendant` int(10) unsigned NOT NULL DEFAULT 0,
+  `version` int(10) unsigned NOT NULL DEFAULT 1,
+  `rss_title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `CurrentVersion`
---
-
-LOCK TABLES `CurrentVersion` WRITE;
-/*!40000 ALTER TABLE `CurrentVersion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `CurrentVersion` ENABLE KEYS */;
-UNLOCK TABLES;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
