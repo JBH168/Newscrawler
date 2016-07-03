c21008828618981844bbdf1e8d061951f006bbb3
Jonathan Hassler <JBH168@gmail.com>
2016-07-03 15:57:43 +0200

    pep8 for pipeline

--- newscrawler/crawler/pipelines.py
+++ newscrawler/crawler/pipelines.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
-
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy.exceptions import DropItem
-import mysql.connector
 import datetime
 import os.path
+
 import logging
+
+import mysql.connector
+
+from scrapy.exceptions import DropItem
 from newscrawler.config import CrawlerConfig
 
 ################
@@ -33,48 +34,50 @@ class HTMLCodeHandling(object):
 ################
 #
 # Compares the item's age to the current version in the DB.
-# If the difference is greater than deltaTime, then save the newer version.
+# If the difference is greater than delta_time, then save the newer version.
 #
 ###############
+
 
 class RSSCrawlCompare(object):
 
     def __init__(self):
         self.cfg = CrawlerConfig.get_instance()
-        self.deltaTime = self.cfg.section("Crawler")["deltatime"]
-        self.db = self.cfg.section("Database")
+        self.delta_time = self.cfg.section("Crawler")["deltatime"]
+        self.database = self.cfg.section("Database")
 
         # Establish DB connection
         # Closing of the connection is handled once the spider closes
-        self.conn = mysql.connector.connect(host=self.db["host"],
-                                            port=self.db["port"],
-                                            db=self.db["db"],
-                                            user=self.db["username"],
-                                            passwd=self.db["password"],
+        self.conn = mysql.connector.connect(host=self.database["host"],
+                                            port=self.database["port"],
+                                            db=self.database["db"],
+                                            user=self.database["username"],
+                                            passwd=self.database["password"],
                                             buffered=True)
         self.cursor = self.conn.cursor()
 
         # Defined DB query to retrieve the last version of the article
-        self.compareVersions = ("SELECT * FROM CurrentVersion WHERE url=%s")
+        self.compare_versions = ("SELECT * FROM CurrentVersion WHERE url=%s")
 
     def process_item(self, item, spider):
         if spider.name == 'rssCrawler':
             # Search the CurrentVersion table for a version of the article
             try:
-                self.cursor.execute(self.compareVersions, (item['url'],))
+                self.cursor.execute(self.compare_versions, (item['url'],))
             except mysql.connector.Error as err:
-                print("Something went wrong in rss query: {}".format(err))
+                print "Something went wrong in rss query: {}".format(err)
 
             # Save the result of the query. Must be done before the add,
             #   otherwise the result will be overwritten in the buffer
-            oldVersion = self.cursor.fetchone()
+            old_version = self.cursor.fetchone()
 
-            if oldVersion is not None:
-                # Compare the two download dates. index 3 of oldVersion
+            if old_version is not None:
+                # Compare the two download dates. index 3 of old_version
                 #   corresponds to the downloadDate attribute in the DB
-                if (datetime.datetime.strptime(item['downloadDate'],
-                    "%y-%m-%d %H:%M:%S") - oldVersion[3])\
-                    < datetime.timedelta(hours=self.deltaTime):
+                if (datetime.datetime.strptime(
+                        item['downloadDate'], "%y-%m-%d %H:%M:%S") -
+                        old_version[3]) \
+                        < datetime.timedelta(hours=self.delta_time):
                     raise DropItem("Article in DB too recent. Not saving.")
 
         return item
@@ -95,21 +98,21 @@ class DatabaseStorage(object):
     # init database connection
     def __init__(self):
         self.cfg = CrawlerConfig.get_instance()
-        self.db = self.cfg.section("Database")
+        self.database = self.cfg.section("Database")
         # Establish DB connection
         # Closing of the connection is handled once the spider closes
-        self.conn = mysql.connector.connect(host=self.db["host"],
-                                            port=self.db["port"],
-                                            db=self.db["db"],
-                                            user=self.db["username"],
-                                            passwd=self.db["password"],
+        self.conn = mysql.connector.connect(host=self.database["host"],
+                                            port=self.database["port"],
+                                            db=self.database["db"],
+                                            user=self.database["username"],
+                                            passwd=self.database["password"],
                                             buffered=True)
         self.cursor = self.conn.cursor()
 
         # Init all necessary DB queries for this pipeline
-        self.compareVersions = ("SELECT * FROM CurrentVersion WHERE url=%s")
+        self.compare_versions = ("SELECT * FROM CurrentVersion WHERE url=%s")
 
-        self.insertCurrent = ("INSERT INTO CurrentVersion(localPath,\
+        self.insert_current = ("INSERT INTO CurrentVersion(localPath,\
                               modifiedDate,downloadDate,sourceDomain,url,\
                               title, ancestor, descendant, version, rssTitle)\
                               VALUES (%(localPath)s, %(modifiedDate)s,\
@@ -117,7 +120,7 @@ class DatabaseStorage(object):
                               %(title)s, %(ancestor)s, %(descendant)s,\
                               %(version)s, %(rss_title)s)")
 
-        self.insertArchive = ("INSERT INTO ArchiveVersion(localPath,\
+        self.insert_archive = ("INSERT INTO ArchiveVersion(localPath,\
                               modifiedDate,downloadDate,sourceDomain,url,\
                               title, ancestor, descendant, version, rssTitle)\
                               VALUES (%(localPath)s, %(modifiedDate)s,\
@@ -125,7 +128,8 @@ class DatabaseStorage(object):
                               %(title)s, %(ancestor)s, %(descendant)s,\
                               %(version)s, %(rss_title)s)")
 
-        self.deleteFromCurrent = ("DELETE FROM CurrentVersion WHERE url = %s")
+        self.delete_from_current = (
+            "DELETE FROM CurrentVersion WHERE url = %s")
 
     # Store item data in DB.
     # First determine if a version of the article already exists,
@@ -135,53 +139,55 @@ class DatabaseStorage(object):
 
         # Search the CurrentVersion table for a version of the article
         try:
-            self.cursor.execute(self.compareVersions, (item['url'],))
+            self.cursor.execute(self.compare_versions, (item['url'],))
         except mysql.connector.Error as err:
-            print("Something went wrong in query: {}".format(err))
+            print "Something went wrong in query: {}".format(err)
 
         # Save the result of the query. Must be done before the add,
         #   otherwise the result will be overwritten in the buffer
-        oldVersion = self.cursor.fetchone()
+        old_version = self.cursor.fetchone()
 
         # If there is an existing article with the same URL, then move
         #   it to the ArchiveVersion table, and delete it from the
         #   CurrentVersion table
-        if oldVersion is not None:
-            oldVersionList = {
-                'dbID': oldVersion[0],
-                'localPath': oldVersion[1],
-                'modifiedDate': oldVersion[2],
-                'downloadDate': oldVersion[3],
-                'sourceDomain': oldVersion[4],
-                'url': oldVersion[5],
-                'title': oldVersion[6],
-                'ancestor': oldVersion[7],
-                'descendant': oldVersion[8],
-                'version': oldVersion[9],
-                'rss_title': oldVersion[10],}
+        if old_version is not None:
+            old_version_list = {
+                'dbID': old_version[0],
+                'localPath': old_version[1],
+                'modifiedDate': old_version[2],
+                'downloadDate': old_version[3],
+                'sourceDomain': old_version[4],
+                'url': old_version[5],
+                'title': old_version[6],
+                'ancestor': old_version[7],
+                'descendant': old_version[8],
+                'version': old_version[9],
+                'rss_title': old_version[10], }
 
             # Update the version number of the new article
-            item['version'] = (oldVersion[9] + 1)
+            item['version'] = (old_version[9] + 1)
 
             # Update the ancestor attribute of the new article
-            item['ancestor'] = oldVersion[0]
+            item['ancestor'] = old_version[0]
 
             # Delete the old version of the article from the
             # CurrentVerion table
             try:
-                self.cursor.execute(self.deleteFromCurrent, (oldVersion[5], ))
+                self.cursor.execute(
+                    self.delete_from_current, (old_version[5], )
+                    )
                 self.conn.commit()
             except mysql.connector.Error as err:
-                print("Something went wrong in delete: {}".format(err))
+                print "Something went wrong in delete: {}".format(err)
 
             # Add the old version to the ArchiveVersion table
             try:
-                self.cursor.execute(self.insertArchive, oldVersionList)
+                self.cursor.execute(self.insert_archive, old_version_list)
                 self.conn.commit()
             except mysql.connector.Error as err:
-                print("Something went wrong in archive: {}".format(err))
+                print "Something went wrong in archive: {}".format(err)
 
-        currentVersionList = {
+        current_version_list = {
             'localPath': item['localPath'],
             'modifiedDate': item['modifiedDate'],
             'downloadDate': item['downloadDate'],
@@ -191,31 +197,33 @@ class DatabaseStorage(object):
             'ancestor': item['ancestor'],
             'descendant': item['descendant'],
             'version': item['version'],
-            'rss_title': item['rss_title'],}
+            'rss_title': item['rss_title'], }
 
         # Add the new version of the article to
         # the CurrentVersion table
         try:
-            self.cursor.execute(self.insertCurrent, currentVersionList)
+            self.cursor.execute(self.insert_current, current_version_list)
             self.conn.commit()
 
         except mysql.connector.Error as err:
-            print("Something went wrong in commit: {}".format(err))
+            print "Something went wrong in commit: {}".format(err)
 
         logging.info("Article inserted into the database.")
 
-        if oldVersion is not None:
+        if old_version is not None:
             # Retrieve the auto_id from the new article for the old version's
             #   descendant attribute
             try:
                 item['dbID'] = self.cursor.lastrowid
             except mysql.connector.Error as err:
-                print("Something went wrong in id query: {}".format(err))
+                print "Something went wrong in id query: {}".format(err)
 
             # Update the old version's descendant attribute
             try:
-                self.cursor.execute("UPDATE ArchiveVersion SET descendant=%s WHERE\
-                                    id=%s", (item['dbID'], oldVersion[0],))
+                self.cursor.execute(
+                    "UPDATE ArchiveVersion SET descendant=%s WHERE\
+                    id=%s", (item['dbID'], old_version[0],)
+                    )
             except mysql.connector.Error as err:
                 print("Something went wrong in version update: {}"
                       .format(err))
@@ -235,13 +243,11 @@ class DatabaseStorage(object):
 
 class LocalStorage(object):
 
-
-
     # Save the html and filename to the local storage folder
     def process_item(self, item, spider):
 
         # Add a log entry confirming the save
-        logging.info("Saving to %s" % item['absLocalPath'])
+        logging.info("Saving to %s", item['absLocalPath'])
 
         # Ensure path exists
         dir_ = os.path.dirname(item['absLocalPath'])
