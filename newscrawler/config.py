3a52812cfc08f596f73c47c80a5baa7fd67db850
movabo <moritz.bock@uni-konstanz.de>
2016-07-03 23:09:42 +0200

    Improved codequality (pep8) for config

--- newscrawler/config.py
+++ newscrawler/config.py
@@ -47,9 +47,12 @@ class CrawlerConfig(object):
     # Here starts the actual class!
     log = None
     log_output = []
+    sections = None
+    parser = None
     __current_section = None
     __logging = None
     __scrapy_options = None
+    __config = None
 
     def __init__(self):
         """The constructor
@@ -77,12 +80,10 @@ class CrawlerConfig(object):
         self.log_output.append(
             {"level": "info", "msg": "Loading config-file (%s)" % filepath})
         self.load_config()
-        self.handle_general()
         self.handle_logging()
 
     def load_config(self):
-        """Load the config to self.config. Recursive dict:
-           [section][option] = value"""
+        """Load the config-file"""
         self.__config = {}
 
         # Parse sections, its options and put it in self.config.
@@ -99,12 +100,13 @@ class CrawlerConfig(object):
                         .get(section, option)
                     try:
                         self.__config[section][option] = literal_eval(opt)
-                    except (SyntaxError, ValueError) as error:
+                    except (SyntaxError, ValueError):
                         self.__config[section][option] = opt
                         self.log_output.append(
                             {"level": "debug",
                              "msg": "Option not literal_eval-parsable"
-                             " (maybe string): [%s] %s" % (section, option)})
+                                    " (maybe string): [{0}] {1}"
+                                    .format(section, option)})
 
                     if self.__config[section][option] == -1:
                         self.log_output.append(
@@ -120,16 +122,6 @@ class CrawlerConfig(object):
                     )
                     self.__config[section][option] = None
 
-    def get_logging(self):
-        if self.__logging is None:
-            self.__logging = {}
-            logging_options = self.__config["Logging"]
-
-            for key, value in logging_options.iteritems():
-                self.__logging[key.upper()] = value
-
-        return deepcopy(self.__logging)
-
     def get_scrapy_options(self):
         """
         returns all the options listed in the config section 'Scrapy'
@@ -141,9 +133,6 @@ class CrawlerConfig(object):
             for key, value in options.iteritems():
                 self.__scrapy_options[key.upper()] = value
         return self.__scrapy_options
-
-    def handle_general(self):
-        return
 
     def handle_logging(self):
         """To allow devs to log as early as possible, logging will already be handled here"""
@@ -163,15 +152,38 @@ class CrawlerConfig(object):
                 self.log.debug(msg["msg"])
 
     def config(self):
+        """
+        Get the whole config as a dict.
+
+        :returns: The whole config as dict[section][option] (all lowercase)
+        :rtype: dict
+        """
         return deepcopy(self.__config)
 
     def section(self, section):
+        """
+        Get the whole section of a the config.
+
+        :param section (string): The section to get all the options from.
+        :return dict[option] (all lowercase)
+        """
         return deepcopy(self.__config[section])
 
     def set_section(self, section):
+        """
+        Sets the current section to get the options from.
+
+        :param section (string)
+        """
         self.__current_section = section
 
     def option(self, option):
+        """
+        Gets the option, set_section needs to be set before.
+
+        :param option (string): The option to get.
+        :return mixed: The option from from the config.
+        """
         if self.__current_section is None:
             raise RuntimeError('No section set in option-getting')
         return self.__config[self.__current_section][option]
@@ -223,15 +235,35 @@ class JsonConfig(object):
         self.load_json(filepath)
 
     def load_json(self, filepath):
+        """
+        Loads the JSON-file from the filepath.
+
+        :param filepath (string): The location of the JSON-file.
+        """
         self.__json_object = json.load(open(filepath, 'r'))
 
     def config(self):
+        """
+        Get the whole JSON as a dict.
+
+        :return dict
+        """
         return deepcopy(self.__json_object)
 
     def get_site_objects(self):
+        """
+        Get the object containing all sites.
+
+        :return sites (dict): The sites from the JSON-file
+        """
         return deepcopy(self.__json_object["base_urls"])
 
     def get_url_array(self):
+        """
+        Get all url-objects in an array
+
+        :return sites (array): The sites from the JSON-file
+        """
         urlarray = []
         for urlobjects in self.__json_object["base_urls"]:
             urlarray.append(urlobjects["url"])
